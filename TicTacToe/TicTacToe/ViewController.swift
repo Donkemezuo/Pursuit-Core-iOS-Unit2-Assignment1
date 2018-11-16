5c154fd289f31e44cf6c6d8a8d80524093c06148
Donkemezuo <raymonddonkemezuo.org>
2018-11-15 22:29:01 -0500

    My progress so far

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -11,46 +11,138 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var gameLabel: UILabel!
+    @IBOutlet weak var scoresBoard:UILabel!
     
-
-    var activeGame = true
-    var myGameStatus = [0,0,0,0,0,0,0,0,0]
-    let winningcondition = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-
+   var firstPlayerScore = 0
+    var secondPlayerScore = 0
+    
+    var myGameBoard = [
+                        ["","",""],
+                        ["","",""],
+                        ["","",""]
+    ]
     var activePlayer = 1
-    @IBAction func MyGameButton(_ sender: UIButton) {
-        let activePosition = sender.tag 
-        if myGameStatus[activePosition] == 0 {
-            
-        myGameStatus[activePosition] = activePlayer
-            
-            if activePlayer == 1 {
-        sender.setImage(UIImage(named: "Nought"), for: [])
-        
-            activePlayer = 2
-        } else {
-            sender.setImage(UIImage(named: "Cross"), for: [])
-        activePlayer = 1
-        }
-   
-            for combination in winningcondition {
-                if myGameStatus[combination[0]] == 0 && myGameStatus[combination[0]] == myGameStatus[combination[1]] && myGameStatus[combination[1]] == myGameStatus[combination[2]] {
-            activeGame = false
     
-                   print(myGameStatus[combination[0]])
-        func viewDidLoad() {
+    func myRowWinningCondition(arr:[[String]]) {
+        for row in myGameBoard {
+            if row == ["X", "X", "X"] {
+                firstPlayerScore += 1
+                scoresBoard.text = """
+                player 1 score = \(firstPlayerScore)
+                player 2 score = \(secondPlayerScore)
+                """
+                print("player 1 won")
+            } else if row == ["O", "O", "O"] {
+                secondPlayerScore += 1
+                scoresBoard.text = """
+                player 1 score = \(firstPlayerScore)
+                player 2 score = \(secondPlayerScore)
+                """
+                print("player 2 won")
+            }
+    }
+    }
+    func myColumnWinningCondition(arr: [[String]]) {
+        for columnIndex in 0..<arr[0].count {
+            if arr[0][columnIndex] == "X" && arr[1][columnIndex] == "X" && arr[2][columnIndex] == "X" {
+                firstPlayerScore += 1
+                scoresBoard.text = """
+                player 1 score = \(firstPlayerScore)
+                player 2 score = \(secondPlayerScore)
+                """
+                print("Player 1 won")
+            } else if arr[0][columnIndex] == "O" && arr[1][columnIndex] == "O" && arr[2][columnIndex] == "O"{
+                secondPlayerScore += 1
+                scoresBoard.text = """
+                player 1 score = \(firstPlayerScore)
+                player 2 score = \(secondPlayerScore)
+                """
+                print("player 2 won")
+            }
+        }
+    }
+    func myFirstDiagonalWinningCondition(arr:[[String]]) {
+        var myFirstDiagonal = [String]()
+        for indexString in 0..<arr.count{
+            myFirstDiagonal.append(arr[indexString][indexString])
+            if myFirstDiagonal == ["X","X","X"]{
+                firstPlayerScore += 1
+                scoresBoard.text = """
+                player 1 score = \(firstPlayerScore)
+                player 2 score = \(secondPlayerScore)
+                """
+                print("player 1 won")
+            } else if myFirstDiagonal == ["O","O","O"]{
+                secondPlayerScore += 1
+                scoresBoard.text = """
+                player 1 score = \(firstPlayerScore)
+                player 2 score = \(secondPlayerScore)
+                """
+                print("player 2 won")
+    }
+        }
+    }
+    func mySecondDiagonalWinningCondition(arr:[[String]]){
+        var mySecondDiagonal = [String]()
+        for index in 0..<arr.count{
+            mySecondDiagonal.append(arr[index][arr.count - 1 - index])
+            if mySecondDiagonal == ["X","X","X"]{
+                firstPlayerScore += 1
+                scoresBoard.text = """
+                player 1 score = \(firstPlayerScore)
+                player 2 score = \(secondPlayerScore)
+                """
+                print("player 1 won")
+            } else if mySecondDiagonal == ["O","O","O"] {
+                secondPlayerScore += 1
+                scoresBoard.text = """
+                player 1 score = \(firstPlayerScore)
+                player 2 score = \(secondPlayerScore)
+                """
+                print("player 2 won")
+            }
+    }
+    }
+    
+    func myGameBoardUpdate(currentLocation:(Int,Int)){
+        if activePlayer == 1 {
+            myGameBoard[currentLocation.0][currentLocation.1] = "X"
+            myRowWinningCondition(arr: myGameBoard)
+            myColumnWinningCondition(arr: myGameBoard)
+            myFirstDiagonalWinningCondition(arr: myGameBoard)
+            mySecondDiagonalWinningCondition(arr: myGameBoard)
+             activePlayer = 2
+        } else if activePlayer == 2 {
+            myGameBoard[currentLocation.0][currentLocation.1] = "O"
+            myColumnWinningCondition(arr: myGameBoard)
+            myRowWinningCondition(arr: myGameBoard)
+            myFirstDiagonalWinningCondition(arr: myGameBoard)
+            mySecondDiagonalWinningCondition(arr: myGameBoard)
+            activePlayer = 1
+    }
+    }
+@IBAction func MyGameButton(_ sender: GameButton) {
+        let currentPosition = (sender.row,sender.col)
+   myGameBoardUpdate(currentLocation: currentPosition)
+   print(myGameBoard)
+    }
+override func viewDidLoad() {
     super.viewDidLoad()
 
         
 
    
     // Do any additional setup after loading the view, typically from a nib.
-  }
+  
 
 
 }
+    }
 
-}
-}
-}
-}
+
+
+
+//}
+//}
+//}
+
